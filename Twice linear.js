eb748b646c78c5f2de7356fb06e33415d81d30d0
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-04-27 00:44:17 +0300

    Twice linear

---
+++ Twice linear.js
@@ -0,0 +1,51 @@
+function dblLinear(n) {
+    let arrayU = [1];
+
+    //cursors
+    let x = 0;
+    let y = 0;
+    //potential values
+    let nextX = 0;
+    let nextY = 0;
+
+    for(let i = 0 ; i < n; i++){
+        //choice next value nextX or nextY
+        nextX = 2 * arrayU[x] + 1;
+        nextY = 3 * arrayU[y] + 1;
+        if(nextX<=nextY){
+            //shift cursor if needed
+            x++;
+
+            //push minimal value to arrayU
+            arrayU.push(nextX);
+            if(nextX===nextY){
+                y++;
+            }
+        }else{
+            y++;
+            arrayU.push(nextY);
+        }
+    }
+
+    return arrayU[n];
+}
+
+console.log(dblLinear(100));
+
+
+/////////////////////////////////////////
+//best practice
+
+function dblLinear2(n) {
+
+    var u = [1], pt2 = 0, pt3 = 0; //two pointer
+
+    for(var i = 1;i<=n;i++){
+        u[i] = Math.min(2* u[pt2] + 1, 3*u[pt3] + 1);
+        if(u[i] == 2 * u[pt2] + 1) pt2++;
+        if(u[i] == 3 * u[pt3] + 1) pt3++;
+    }
+
+    return u[n];
+
+}
