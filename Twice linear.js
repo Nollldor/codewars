fb10d3d8df56efaad06e41c0c4d7c85f762a345c
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-04-27 00:44:42 +0300

    Twice linear(fixed)

--- Twice linear.js
+++ Twice linear.js
@@ -38,9 +38,9 @@ console.log(dblLinear(100));
 
 function dblLinear2(n) {
 
-    var u = [1], pt2 = 0, pt3 = 0; //two pointer
+    let u = [1], pt2 = 0, pt3 = 0; //two pointer
 
-    for(var i = 1;i<=n;i++){
+    for(let i = 1;i<=n;i++){
         u[i] = Math.min(2* u[pt2] + 1, 3*u[pt3] + 1);
         if(u[i] == 2 * u[pt2] + 1) pt2++;
         if(u[i] == 3 * u[pt3] + 1) pt3++;
